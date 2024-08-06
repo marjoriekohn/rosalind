dbf9e726da0186821bac9b8c650b82829611a990
Marjorie Kohn <marjoriekohn01@gmail.com>
2024-08-06 19:37:17 -0400

    add test for dna_to_rna.py

--- RNA/dna_to_rna.py
+++ RNA/dna_to_rna.py
@@ -7,9 +7,14 @@
 def dna_to_rna(dna_string):
     rna_string = []
 
+    if dna_string == '':
+        return 'Empty DNA string'
+
     for nucleotide in dna_string:
         if nucleotide == 'T':
             rna_string.append('U')
-        else:
+        elif nucleotide in 'AGC':
             rna_string.append(nucleotide)
+        else:
+            return "Invalid nucleotide in string"
     return ''.join(rna_string)
---
+++ RNA/test_dna_to_rna.py
@@ -0,0 +1,18 @@
+import unittest
+from dna_to_rna import dna_to_rna
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_valid_nucleotides(self):
+        self.assertEqual(dna_to_rna("ACGT"), "ACGU")  # valid nucleotides present
+
+    def test_invalid_nucleotides(self):
+        self.assertEqual(dna_to_rna("TXACG"), "Invalid nucleotide in string")  # only T nucleotides present
+
+    def test_empty_nucleotides(self):
+        self.assertEqual(dna_to_rna(""), "Empty DNA string")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
