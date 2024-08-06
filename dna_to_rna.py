51ea350f3210223ecb6d18b0db64bd77904615f2
Marjorie Kohn <marjoriekohn01@gmail.com>
2024-08-06 18:45:22 -0400

    solution to problems 1-3

---
+++ dna_to_rna.py
@@ -0,0 +1,15 @@
+# Rosalind #2
+# An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+# Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
+# Given: A DNA string t having length at most 1000 nt.
+# Return: The transcribed RNA string of t.
+
+def dna_to_rna(dna_string):
+    rna_string = []
+
+    for nucleotide in dna_string:
+        if nucleotide == 'T':
+            rna_string.append('U')
+        else:
+            rna_string.append(nucleotide)
+    return ''.join(rna_string)
---
+++ reverse_complement.py
@@ -0,0 +1,15 @@
+# Rosalind #3
+# In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+# The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+# Given: A DNA string s of length at most 1000 bp.
+# Return: The reverse complement sc of s.
+
+def reverse_complement(dna_string):
+    reverse_complement_dna = []
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+
+    for nucleotide in dna_string:
+        if nucleotide in complement:
+            reverse_complement_dna.append(complement[nucleotide])
+
+    return ''.join(reversed(reverse_complement_dna))
