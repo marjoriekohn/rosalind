e815ef3f57c3f422a27c611d1f7d49cdba397ef0
Marjorie Kohn <marjoriekohn01@gmail.com>
2024-08-07 10:58:42 -0400

    completed point_mutations.py

---
+++ HAMM/test_point_mutations.py
@@ -0,0 +1,17 @@
+import unittest
+from point_mutations import point_mutations
+
+
+class MyTestCase(unittest.TestCase):
+    def test_0_hamming_distance(self):
+        self.assertEqual(point_mutations("ACGT", "ACGT"), 0)  # no difference
+
+    def test_1_hamming_distance(self):
+        self.assertEqual(point_mutations("ACGT", "TCGT"), 1)  # one difference
+
+    def test_different_lengths(self):
+        self.assertEqual(point_mutations("ACGTACGT", "ACGT"), "DNA strings are not equal length")  # dna strings of different length
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
